189a118ea71de9870f1e21cb3a634807b4f8a3ce
eminyoung <minyoung.emily@gmail.com>
2025-05-17 15:08:11 -0700

    delete directional light

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
-scene.add(dirLight);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
